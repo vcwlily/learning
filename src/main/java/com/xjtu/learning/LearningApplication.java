ef883089b3b88eef7e36f1b535fdd737a4b9ef64
lily <1006421003@qq.com>
2024-06-15 00:27:25 +0800

    2-2 init

--- src/main/java/com/xjtu/learning/LearningApplication.java
+++ src/main/java/com/xjtu/learning/LearningApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearningApplication {
 
 	public static void main(String[] args) {
+		System.out.println("learning start");
 		SpringApplication.run(LearningApplication.class, args);
 	}
 
